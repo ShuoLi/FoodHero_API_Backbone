0225b9225f24ed4040e8fc66e685a81b6edbe639
Shuo Li <dantein88@gmail.com>
2014-02-20 02:22:29 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Herofood::Application.config.secret_key_base = '79ef05d63e35940045f78b3b532109ce6f014606ba5f4e436b8badd377fa4de4bd3ff320ba4930ae56aae0a34607fd550a41ccf61730711e1fbdbcb1ff8991c9'
